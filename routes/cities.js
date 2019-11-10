6fa6ce354340b62b7efb0765efd457899b1471f8
valentinaloreto <vloretocastillo@gmail.com>
2019-11-10 11:55:10 +0100

    added show route to cities

--- routes/cities.js
+++ routes/cities.js
@@ -2,16 +2,15 @@
 const cities = require('express').Router();
 const cityModel = require('../model/CityModel')
 
-cities.get('/all', (req, res) => {
+const retrieveAllCities = (req, res) => {
     cityModel.find({})
-        .then(files => {
-            res.send(files)
-        })
-        .catch(err => console.log(err));
-});
+    .then(files => {
+        res.send(files)
+    })
+    .catch(err => console.log(err));
+}
 
-cities.post('/', (req, res) => {
-
+const createOneCity = (req,res) => {
     cityModel.find({name : req.body.name})
         .then((file)=>{
             if (file.length === 0) {
@@ -27,19 +26,9 @@ cities.post('/', (req, res) => {
             }
         }) 
         .catch(err => console.log(err));
-});
+}
 
-cities.get('/:id', (req, res) => {
-    // cityModel.find({_id : req.params.id})
-    //     .then((file)=> {
-    //         if (file) {
-    //             res.send(file)
-    //         } else {
-    //             res.send('not found')
-    //         }
-    //     })
-    //     .catch(err => console.log(err));
-
+const retrieveOneCity = (req, res) => {
     cityModel.find({_id : req.params.id})  // .findById(req.params.id)
         .then((file) => {
             res.send(file);  
@@ -54,15 +43,24 @@ cities.get('/:id', (req, res) => {
                 message: "Error retrieving note with id " + req.params.id
             });
         });
+}
 
-});
-
-cities.put('/:id', (req, res) => {
+const updateOneCity = (req, res) => {
     
-});
+}
 
-cities.delete('/:id', (req, res) => {
-    
-});
+const deleteOneCity = (req, res) => {
+
+}
+
+cities.get('/all', (req,res) => retrieveAllCities(req,res));
+
+cities.post('/', (req, res) => createOneCity(req,res));
+
+cities.get('/:id', (req, res) => retrieveOneCity(req, res));
+
+cities.put('/:id', (req, res) => updateOneCity(req, res));
+
+cities.delete('/:id', (req, res) => deleteOneCity(req, res));
 
 module.exports = cities;
